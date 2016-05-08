a4c628d34a9d34f5d21f332697102131793e7ea7
Casey Gollan <casey@bullshit.systems>
2016-05-08 11:02:14 -0400

    typo

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,7 +2,7 @@
 </div><!--/#main-->
 
         <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
-        <script src="/assets/js/jquery.SmoothState.min.js"></script>
+        <script src="/assets/js/jquery.smoothState.min.js"></script>
         <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
         <script src="/assets/js/plugins.js"></script>
         <script src="/assets/js/main.js"></script>
