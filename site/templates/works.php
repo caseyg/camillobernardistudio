4e32da4bb4ab5d1505acfd0d022a98026c917979
Casey Gollan <casey@bullshit.systems>
2016-06-20 16:09:54 -0400

    smaller thumbs on works page

--- site/templates/works.php
+++ site/templates/works.php
@@ -5,7 +5,7 @@
 <div class="drawer drawer--open scene-drawer">
 	<div>
 		<?php foreach($pages->find('/works')->children() as $subpage): ?>
-			<a class="WorksThumb" href="<?php echo $subpage->url() ?>" style="background-image:url('<?php echo $subpage->images()->first()->url() ?>');">
+			<a class="WorksThumb" href="<?php echo $subpage->url() ?>" style="background-image:url('<?php echo $subpage->images()->first()->resize(400)->url() ?>');">
 				<span><?php echo $subpage->title() ?></span>
 			</a>
 		<?php endforeach ?>
