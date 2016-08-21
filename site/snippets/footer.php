9577c47c8118052ab32651a70356d76f2fca1660
Casey Gollan <hello@caseyagollan.com>
2016-08-20 23:26:42 -0400

    goog analytics

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,3 +1,12 @@
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-81833057-1', 'auto');
+    ga('send', 'pageview');
+  </script>
 </div> <!-- .m-scene #smoothstate -->
 
 <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
@@ -7,17 +16,6 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/jquery.ScrollMagic.min.js"></script>
 <script src="/assets/js/plugins.js"></script>
-
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID.
-<script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='https://www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
--->
 
 </body>
 </html>
